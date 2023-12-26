ae4f56ec09add1c588084be97110612b528fa71d
AndreyKolosovKir <andrey.e.04@gmail.com>
2023-12-26 12:59:10 +0300

    new commit. uncommented the tests

--- src/test/java/ru/netology/repository/UserRepositoryTest.java
+++ src/test/java/ru/netology/repository/UserRepositoryTest.java
@@ -1,30 +1,30 @@
-//package ru.netology.repository;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static ru.netology.TestData.*;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//class UserRepositoryTest {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    //ищем юзера
-//    @Test
-//    void findByUsername() {
-//        //предварительно нотовим тестовые данные
-//        userRepository.deleteAll();
-//        userRepository.save(USER_1);
-//        //ищем юзера и сравниваем
-//        assertEquals(USER_1, userRepository.findByUsername(USERNAME_1));
-//    }
-//}
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.netology.DataForTest.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //ищем юзера
+    @Test
+    void findByUsername() {
+        //предварительно нотовим тестовые данные
+        userRepository.deleteAll();
+        userRepository.save(USER_1);
+        //ищем юзера и сравниваем
+        assertEquals(USER_1, userRepository.findByUsername(USERNAME_1));
+    }
+}
